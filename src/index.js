106457069c431b1ce6e4bc5276755424e05f6eac
Antonin06 <avon.antonin@gmail.com>
2020-08-24 10:23:26 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Configurator from './Components/Configurator';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import store from "./js/store/index";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Configurator />
+  </Provider>,
   document.getElementById('root')
 );
 
