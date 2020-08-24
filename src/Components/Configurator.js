3ffe3753f84ac6027761a6a2301092d2713959ef
Antonin06 <avon.antonin@gmail.com>
2020-08-24 14:44:32 +0200

    config+

--- src/Components/Configurator.js
+++ src/Components/Configurator.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import logo from '../logo.svg';
-import '../App.css';
-import { Navbar } from 'react-bootstrap'
+import '../custom.css';
+import { Navbar, Breadcrumb, Jumbotron, Container } from 'react-bootstrap'
+import logoBlue from '../logo.png'
 
 const Configurator = () => (
-
-  <Navbar bg="dark">
+<>
+  <Navbar bg="white" className="d-flex justify-content-center">
       <Navbar.Brand href="#home">
         <img
-          src="/logo.svg"
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
+          src={logoBlue}
+          className="mt-3 mb-3 App-logo"
           alt="React Bootstrap logo"
         />
       </Navbar.Brand>
     </Navbar>
 
+    <Jumbotron fluid className="p-4 mb-0 sticky-top bg-black text-white">
+  <Container className="d-inline-flex">
+    <p className="mb-0">
+      Alpine A110
+    </p>
+    <p className="mb-0 align-right">
+      €€€€€
+    </p>
+  </Container>
+</Jumbotron>
+
+<Breadcrumb>
+  <Breadcrumb.Item href="#">Version</Breadcrumb.Item>
+  <Breadcrumb.Item href="">Couleurs</Breadcrumb.Item>
+  <Breadcrumb.Item >Jantes</Breadcrumb.Item>
+  <Breadcrumb.Item >Scellerie</Breadcrumb.Item>
+  <Breadcrumb.Item >Equipements</Breadcrumb.Item>
+  <Breadcrumb.Item >Accessoires</Breadcrumb.Item>
+
+</Breadcrumb>
+</>
 )
 
 export default Configurator;
